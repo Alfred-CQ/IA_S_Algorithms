b4f65f1e6a87a1b4186b8f374d5823f31e5eb94e
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-08-30 04:11:32 -0500

    Add random coordinates csv file creator

---
+++ utils/random_coords_creator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#define LIMIT_X 100
+#define LIMIT_Y 100
+#define N_NODES 26
+
+struct Node{
+    int x, y;
+    Node(int _x, int _y)
+        : x(_x), y(_y)
+    {}
+};
+
+
+int main(){
+    srand (time(NULL));
+    vector<Node> nodes;
+    
+    string file_name = "random_coords.csv";
+
+    ofstream out(file_name);
+    out<<"x,y\n";
+    for(int i = 0; i < N_NODES; i++)
+    {
+        int _x = -1 * (rand() % 2 ? -1 : 1) * rand() % LIMIT_X;
+        int _y = -1 * (rand() % 2 ? -1 : 1) * rand() % LIMIT_Y;
+        Node temp(_x, _y);
+        nodes.push_back(temp);
+    }
+
+    for(int i = 0; i < N_NODES; i++)
+    {
+        out << nodes[i].x << "," << nodes[i].y << "\n";
+    }
+    
+    out.close();
+}
