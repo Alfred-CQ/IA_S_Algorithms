41f03e1d771d262aeb348356f7a5011eff5d0d4e
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-09-05 10:57:36 -0500

    Fix path to target and visited nodes vector Path creating non-existed edges Visited nodes wasn't a reference value

--- src/DFS.hpp
+++ src/DFS.hpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
 
 #include "../utils/csv_files_reader.h" 
 #include "../utils/graph.h"
 
 bool found = false;
 
-void DFS(IA_NODES nodes,IA_ADJ_LIST adj_list, vector<bool> visited, int cur, int end, string& EXP, string& PATH)
+void DFS(IA_NODES nodes,IA_ADJ_LIST adj_list, vector<bool>& visited, int cur, int end, string& EXP, string& PATH, vector<int>& node_path)
 { 
     visited[cur] = 1;
     
-    PATH.push_back(char(cur+65));
-    PATH.push_back('\n');
-    
-    cout<<char(cur+65)<<"->";
-    
     if(cur==end)
     {
-        cout << "END\nFound!\n";
+        cout << "Found!\n";
         found = true;
         return;
     }
@@ -31,8 +27,8 @@ void DFS(IA_NODES nodes,IA_ADJ_LIST adj_list, vector<bool> visited, int cur, int
             EXP.push_back(',');
             EXP.push_back(char(adj_list[cur][i].id + 65));
             EXP.push_back('\n');
-
-            DFS(nodes,adj_list,visited,adj_list[cur][i].id,end, EXP, PATH);
+            node_path[adj_list[cur][i].id] = cur;
+            DFS(nodes,adj_list,visited,adj_list[cur][i].id,end, EXP, PATH, node_path);
         }
         
     }
@@ -45,19 +41,43 @@ void DFS_t(Graph* G, int beg, int end)
 
     ofstream out(file_name);
     ofstream out_expn(file_exp);
-    
+    vector<int> node_path(N_NODES, -1);
     vector<bool> visited(G->nodes.size(),0);
     found = false;
     
     cout << " -- DFS algorithm --" << '\n';
 
-    DFS(G->nodes,G->adj_list,visited,beg,end,EXP,PATH);
+    DFS(G->nodes,G->adj_list,visited,beg,end,EXP,PATH, node_path);
     
+    stack<int> s;
+
+    int pos = end;
+
+    do
+    {
+        s.push(pos);
+        pos = node_path[pos];
+    }while(pos != -1);
+
+    string dfs_path;
+
+    while(!s.empty())
+    {
+        dfs_path.push_back(get_node_name(s.top()));
+        dfs_path.push_back('\n');
+        s.pop();
+    }
+
+    for(int i = 0; i < dfs_path.size(); i+=2){
+        cout << dfs_path[i] << "->";
+    }
+    cout<<"END\n";
+
     out_expn<<"from,to\n";
     out_expn<<EXP;
-
+    
     out<<"label\n";
-    out<<PATH;
+    out<<dfs_path;
 
     out.close();
     out_expn.close();
